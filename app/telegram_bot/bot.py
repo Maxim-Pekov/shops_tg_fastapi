c49a0dfe646390d98c56cb94839620bf3ce721d1
max <max.pekov@gmail.com>
2025-03-26 09:39:18 +0300

    feature_0007 Added aiogram telegram bot.

--- app/telegram_bot/bot.py
+++ app/telegram_bot/bot.py
@@ -1,9 +1,55 @@
-import os
-import datetime
+import asyncio
+import logging
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import requests
 from aiogram import Bot, types
-from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
+from aiogram.dispatcher import Dispatcher
 
-bot = Bot(token='your_bot_token')
+token = '8187841647:AAGsJS357kZrcfDVUp-1Ty67fpcSMmf_lN4'
+# bot = Bot(token=token)
+
+
+bot = Bot(token=token)
 dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    await message.reply("Привет!\nНапиши мне названия продуктов которые ты хочешь купить!")
+
+
+@dp.message_handler(commands=['help'])
+async def process_help_command(message: types.Message):
+    await message.reply("Напиши мне что-нибудь, и я отпрпавлю этот текст тебе в ответ!")
+
+
+@dp.message_handler()
+async def echo_message(msg: types.Message):
+    response = requests.get(
+        f'http://127.0.0.1:8000/products/search_by_name/{msg.text}',
+    )
+    response.raise_for_status()
+    products = [f"{product.get('name')}\n" for product in response.json()]
+    # await bot.send_message(msg.from_user.id, products)
+    if products:
+        # Берем первый продукт из списка
+        first_product = response.json()[0].get('name')
+
+        # Создаем инлайн-кнопку
+        keyboard = InlineKeyboardMarkup()
+        button = InlineKeyboardButton(text=first_product, callback_data=f"product_1")
+        keyboard.add(button)
+
+        # Отправляем сообщение с кнопкой
+        await bot.send_message(
+            msg.from_user.id,
+            "Вот первый продукт из найденных:",
+            reply_markup=keyboard
+        )
+    else:
+        # Если продуктов нет, отправляем сообщение об этом
+        await bot.send_message(msg.from_user.id, "Продукты не найдены.")
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
